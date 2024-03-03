37dbee7e7abd308a387cb1522774cf15a3c01c0d
Zhenghong Huang <alanmax@me.com>
2024-03-03 19:25:19 +0800

    First Version

--- main.py
+++ main.py
@@ -16,39 +16,88 @@ from googleapiclient.errors import HttpError
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
 HOMEPAGE = "http://ieda.ust.hk/eng/events.php?catid=6&sid=50"
 SITE = "http://ieda.ust.hk/eng/event_detail.php?type=E&id="
+DEFAULT_TIME_INTERVAL = 1.5
+DEFAULT_ID_FORWAD = 3
 
-def scrapping(service, settings):
-  eventID = settings["eventID"]
+def constructEventBody(event):
+  startTime = datetime.datetime.strptime(event["Date"] + " " + event["Time"].replace(".", "").capitalize(), "%d %B %Y (%A) %I:%M %p")
+  endTime = startTime + datetime.timedelta(hours=DEFAULT_TIME_INTERVAL)
 
-  HOMEPAGE = "http://ieda.ust.hk/eng/events.php?catid=6&sid=50"
+  description = event["Title"] + "\n\nBy " + event["Speaker"]
+  if "School" in event:
+      description += "\nFrom " + event["School"]
+  description += "\n\n" + event["Link"] + "\n\n" + event["Abstract"] + "\n\n" + event["Bio"]
 
-  contents = urllib.request.urlopen(HOMEPAGE).read()
-  soup = BeautifulSoup(contents, 'html.parser')
+  res = {
+    "summary": (event['Type'] + " by " + event["Speaker"]),
+    "location": event["Venue"],
+    "description": description,
+    "start": {
+      "dateTime": startTime.isoformat(),
+      "timeZone": "Asia/Hong_Kong",
+    },
+    "end": {
+      "dateTime": endTime.isoformat(),
+      "timeZone": "Asia/Hong_Kong",
+    },
+    "recurrence": ["RRULE:FREQ=DAILY;COUNT=1"],
+    "reminders": {
+      "useDefault": False,
+      "overrides": [
+        {"method": "popup", "minutes": 15},
+      ],
+    },
+  }
+  return res
 
-  # Get the latest event ID
-  lastEventID = int(soup.find('div', class_='info-block__date active').attrs['data-tab'])
+def adding(service, calendarID, event):
+  try:
+    event = service.events().insert(calendarId=calendarID, body=constructEventBody(event)).execute()
+    logging.info("Event created: %s" % (event.get("htmlLink")))
+  except Exception as error:
+    logging.error(f"Error Occured when Adding: {error}")
 
-  if eventID > lastEventID:
-    logging.info("No new event")
-    return None
-
+def scrapping(eventID):
   contents = urllib.request.urlopen(SITE+str(eventID)).read()
   soup = BeautifulSoup(contents, 'html.parser')
 
-  title = soup.find(class_='context__subtitle').text.strip() #Joint OM/IE Seminar
+  event = dict()
 
-  if title.find("Seminar") == -1:
-    logging.info("Not a seminar")
-    return scrapping(service, eventID+1)
+  eventType = soup.find(class_='context__subtitle').text.strip() #Joint OM/IE Seminar / Seminar / Thesis Examination / etc
+
+  if eventType is None:
+    logging.info("Invalid event at " + str(eventID))
+    return None
+  else:
+    event['Type'] = eventType
+
+  event["Title"] = soup.find(class_='context__title').text.strip() #Title
+  
+  for label, value in zip(soup.find_all('span', class_='item-label'), soup.find_all('span', class_='item-value')):
+    event[label.text] = value.text #Date, Time, Venue, Speaker, etc, School is Optional
+
+  event["Abstract"] = soup.find(class_='context__text abstract').text.strip() #Abstract
+
+  speaker_bio = soup.find(class_='context__text speaker')
+  if speaker_bio is not None:
+    event["Bio"] = speaker_bio.text.strip() #Bio
+  else:
+    logging.info("No Bio for event at " + str(eventID))
+    event["Bio"] = ""
+
+  event["Link"] = SITE+str(eventID) #Link
+
+  return event
 
 
 
 def main():
-  """Shows basic usage of the Google Calendar API.
-  Prints the start and name of the next 10 events on the user's calendar.
-  """
   creds = None
   settings = None
+
+  logging.basicConfig(filename='scrapping.log', level=logging.INFO)
+
+  logging.info("Run at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
   if os.path.exists("settings.json"):
     with open("settings.json", "r") as f:
@@ -67,34 +116,33 @@ def main():
   
   try:
     service = build("calendar", "v3", credentials=creds)
-
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
-    print("Getting the upcoming 10 events")
-    events_result = (
-        service.events()
-        .list(
-            calendarId=settings["calendarId"],
-            timeMin=now,
-            maxResults=10,
-            singleEvents=True,
-            orderBy="startTime",
-        )
-        .execute()
-    )
-    events = events_result.get("items", [])
-
-    if not events:
-      print("No upcoming events found.")
-      return
-
-    # Prints the start and name of the next 10 events
-    for event in events:
-      start = event["start"].get("dateTime", event["start"].get("date"))
-      print(start, event["summary"])
-
   except HttpError as error:
-    print(f"An error occurred: {error}")
+    logging.error(f"Build Service Failed: {error}")
+    return None
+
+  try:
+    contents = urllib.request.urlopen(HOMEPAGE).read()
+  except Exception as error:
+    logging.error(f"Urllib error occurred: {error}")
+    return None
+  
+  soup = BeautifulSoup(contents, 'html.parser')
+  lastEventID = int(soup.find('div', class_='info-block__date active').attrs['data-tab'])
+  first_event = settings['last_eventID'] + 1
+
+  if first_event > lastEventID:
+    logging.info("No new event")
+    return None
+
+  for i in range(first_event, lastEventID + DEFAULT_ID_FORWAD):
+  # Get the latest event ID
+    event = scrapping(i)
+    if event is not None:
+      settings['last_eventID'] = i
+      adding(service, settings["calendarId"], event)
+  
+  with open("settings.json", "w") as f:
+    json.dump(settings, f)
 
 
 if __name__ == "__main__":
